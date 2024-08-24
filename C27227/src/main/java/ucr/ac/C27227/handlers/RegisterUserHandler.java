675915f952510060100fb1d8643a665e8f8556b4
Kendall Sánchez <117062888+Kensc19@users.noreply.github.com>
2024-08-23 19:46:10 -0600

    Laboratorio 2

--- C27227/src/main/java/ucr/ac/C27227/handlers/RegisterUserHandler.java
+++ C27227/src/main/java/ucr/ac/C27227/handlers/RegisterUserHandler.java
@@ -4,8 +4,8 @@ public interface RegisterUserHandler {
     record Command(String name, String email, String password){}
 
     sealed  interface Result {
-        final record Success(String message) implements Result{}
-        final record InvalidData (String ... fields) implements  Result{}
+         record Success(String message) implements Result{}
+         record InvalidData (String ... fields) implements  Result{}
     }
-    void registerUser(Command command);
+    Result registerUser(Command command);
 }
