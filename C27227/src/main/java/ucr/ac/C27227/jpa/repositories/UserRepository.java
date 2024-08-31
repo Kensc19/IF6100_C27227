1c91c3bbc4df833c4a783214f86cd081cb294453
Kendall Sánchez <117062888+Kensc19@users.noreply.github.com>
2024-08-30 19:39:26 -0600

    Laboratorio 3 C27227

--- C27227/src/main/java/ucr/ac/C27227/jpa/repositories/UserRepository.java
+++ C27227/src/main/java/ucr/ac/C27227/jpa/repositories/UserRepository.java
@@ -1,13 +1,22 @@
 package ucr.ac.C27227.jpa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ucr.ac.C27227.jpa.entities.UserEntity;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
+
+    Optional<UserEntity> findByEmail(String email);
+    Long countByEmail(String email);
+    //@Query("SELECT u FROM UserEntity u WHERE u.name = :name")
+    @Query(nativeQuery = true, value = "SELECT * FROM users WHERE name = :name")
+    List<UserEntity> findByName (String name);
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # IF6100_C27227
+
+- Laboratorio 1 Configuración
+- Se crea proyecto y configuran algunos archivos.
+
+
+- Laboratorio 2 
