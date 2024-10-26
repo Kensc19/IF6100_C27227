04065244d7959e3c779694989d57fa2760fa37a8
Kendall Sánchez <117062888+Kensc19@users.noreply.github.com>
2024-10-25 19:34:33 -0600

    Laboratorio 10

--- UI/demo-reactjs/src/main.tsx
+++ UI/demo-reactjs/src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import RegisterUser from './pages/RegisterUser/index.tsx';
+//import RegisterUser from './pages/RegisterUser/index.tsx';
+import Login from './pages/RegisterUser/Login/Login.tsx';
 //import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RegisterUser />
+		<Login />
 	</StrictMode>,
 );
